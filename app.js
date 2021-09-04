b272a85b347fbb82d4ef2c5bafe5f97d3de805f2
Nikhil <nkmnikhil0009@gmail.com>
2021-09-04 15:35:39 +0530

    Added functions

--- app.js
+++ app.js
@@ -1,3 +1,72 @@
+let firstOperand = null
+let secondOperand = null
+let currentOperation = null
+let resetScreen = false
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-Operator');
+const deleteButton = document.getElementById('deletebtn');
+const clearButton = document.getElementById('clearbtn');
+const equalsButton = document.getElementById('equalsbtn');
+const displayScreen = document.getElementById('screen');
+
+clearButton.addEventListener('click' , clear);
+deleteButton.addEventListener('click' , deleteNum);
+equalsButton.addEventListener('click' , evaluate);
+
+function clear() {
+    displayScreen.textContent('0');
+    secondOperand = '';
+    firstOperand = '';
+    currOperation = null;
+}
+
+function appendNumber(num) {
+    if(displayScreen.textContent === '0' || resetScreen) reset();
+    displayScreen.textContent += num; 
+}
+
+function reset() {
+    displayScreen.textContent = '';
+    resetScreen = false;
+}
+
+function deleteNum() {
+    displayScreen.textContent = displayScreen.textContent.toString().slice(0,-1);
+}
+
+function setOperation(operator) {
+    if (currentOperation !== null) evaluate()
+    firstOperand = displayScreen.textContent
+    currentOperation = operator
+    resetScreen = true
+}
+  
+function evaluate() {
+    if (currentOperation === null || resetScreen) return
+    if (currentOperation === '/' && displayScreen.textContent === '0') {
+        alert("ERROR : Division by 0 not allowed")
+        return
+    }
+    secondOperand = displayScreen.textContent
+    displayScreen.textContent = roundResult(
+        operate(currentOperation, firstOperand, secondOperand)
+    )
+    currentOperation = null
+}
+
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000
+}
+
+numberButtons.forEach((button) =>
+  button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
+operatorButtons.forEach((button) =>
+  button.addEventListener('click', () => setOperation(button.textContent))
+)
+
 function add(a , b){
     return a + b;
 }
